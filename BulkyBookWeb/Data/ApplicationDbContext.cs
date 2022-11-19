1e74d0a1e2b224d5545daa8ed70b66a80320c63b
Chethan M <chethan.m-dxc@hpe.com>
2022-11-19 14:30:06 +0530

    Add project files.

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        //#1- EF
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        //#2 - EF
+        public DbSet<Category> Categories { get; set; }
+    }
+}
